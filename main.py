8fe94abb83a6588d743731ac935652ae571d81da
Roman Simmons <simmorom000@warren.k12.in.us>
2024-10-21 08:28:01 -0400

    github

---
+++ main.py
@@ -0,0 +1,13 @@
+# TODO: Wrote something in the
+#  input function
+#  instructing the user to enter their age.
+age = int(input(""))
+
+# TODO: Use a relational operator to evaluate is_eligible
+#  YOU are are eligible if your age is greater than or equal
+#  to 18.
+is_eligible = 18 or older
+
+#TODO: Print the result
+
+
